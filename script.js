074e43ed8801ede25b50829e62e1169f68c824cb
Gaston Gimenez <gaston.ignacio.gimenez@gmail.com>
2023-04-03 11:54:58 -0300

    se agrega confirmacion de mensaje, telefono en contacto, y mejoras varias

--- script.js
+++ script.js
@@ -4,19 +4,31 @@ window.addEventListener('load', () => {
     const submitButton = document.querySelector('#submit');
     submitButton?.addEventListener('click', (event) => {
         event.preventDefault();
+        document.querySelector('#error').classList.remove('show-error');
+        document.querySelector('#ok').classList.remove('show-ok');
         const name = document.querySelector('#name').value;
         const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
         const message = document.querySelector('#message').value;
-
         if (name !== '' && email !== '' && message !== '') {
             // ok
             document.querySelector('#user-name').innerHTML = name;
             document.querySelector('#user-email').innerHTML = email;
+            document.querySelector('#user-telefono').innerHTML = telefono;
             document.querySelector('#user-message').innerHTML = message;
+            document.querySelector('#ok').classList.add('show-ok');
         } else {
             // error
+            document.querySelector('#user-name').innerHTML = '';
+            document.querySelector('#user-email').innerHTML = '';
+            document.querySelector('#user-telefono').innerHTML = '';
+            document.querySelector('#user-message').innerHTML = '';
             document.querySelector('#error').classList.add('show-error');
         }
+        document.querySelector('#name').value = '';
+        document.querySelector('#email').value = '';
+        document.querySelector('#telefono').value = '';
+        message = document.querySelector('#message').value = '';
     });
 
     document.querySelector('#get-user')?.addEventListener('click', getUser);
